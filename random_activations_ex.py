5039e33dcf77e53e50dc78c8602b506e8a3f1a49
Anthony Le <leanth@oregonstate.edu>
2019-12-24 20:18:02 -0800

    Print observations, last reward, last move

--- random_activations_ex.py
+++ random_activations_ex.py
@@ -1,6 +1,12 @@
 from osim.env import L2M2019Env
 
 env = L2M2019Env(visualize=True)
+
 observation = env.reset()
+
 for i in range(200):
     observation, reward, done, info = env.step(env.action_space.sample())
+
+print(observation)
+print(f'Last reward: {reward}')
+print(f'Last move: {done}')
